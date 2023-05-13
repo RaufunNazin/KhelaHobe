aaa792a8592216c5c2cb32288138e1b3247c87a7
Raufun <raufun.nazin13@gmail.com>
2023-05-13 23:44:11 +0600

    register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/">
           <Route path="login" element={<Login />}></Route>
+          <Route path="register" element={<Registration />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -2,15 +2,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const nav = useNavigate();
+const Registration = () => {
+    const nav = useNavigate();
   return (
     <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Sign in to your account
+              Create your account
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
@@ -43,25 +43,38 @@ const Login = () => {
                   required=""
                 />
               </div>
-              <div className="text-sm font-medium text-blue-600 hover:underline">
-                Forgot password?
+              <div>
+                <label
+                  htmlFor="cpassword"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  name="cpassword"
+                  id="cpassword"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                  required=""
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="text-sm font-medium text-blue-600 hover:underline">
+                  Forgot password?
+                </div>
               </div>
               <button
                 type="submit"
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
               >
-                Sign in
+                Sign up
               </button>
               <div className="flex justify-between">
                 <p className="text-sm font-light text-gray-500">
-                  Need an account?
+                  Already have an account?
                 </p>
-                <button
-                  type="button"
-                  onClick={() => nav("/register")}
-                  className="font-medium text-blue-600 hover:underline"
-                >
-                  Sign up
+                <button type="button" onClick={() => nav("/login")} className="font-medium text-blue-600 hover:underline">
+                  Sign in
                 </button>
               </div>
             </form>
@@ -72,4 +85,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Registration;
