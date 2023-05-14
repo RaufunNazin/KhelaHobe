569b11954aab02e7f7e30867f02546be6931e575
Prec1se <imainul494@gmail.com>
2023-05-15 01:23:46 +0600

    registered

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import { useLocation } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import user from "../data/user.json";
 
 const Home = () => {
   const location = useLocation();
@@ -18,6 +19,7 @@ const Home = () => {
     }
   }, []);
   const notify = () => {
+    localStorage.setItem("user", JSON.stringify(user));
     var options = {
       body: "You have a Carrom match of round SemiFinals at 2:30pm today",
       icon: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/329016851_1126132004719742_309882006475339599_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEgCZrToptI2f1PWX1K9JB1jupAhNR8NY-O6kCE1Hw1j2nkyLak7IUMemig5yUAP-lW4MgimqaPxHMLW30JRS1v&_nc_ohc=4dhUlaRHld4AX_GenX-&_nc_ht=scontent.fdac31-1.fna&oh=00_AfCpaOu-tWaUmMRHEUZx1vsUcYl7-04bRVZFtlIm5vhy-Q&oe=6465453E",
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,24 +1,82 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
+import user from "../data/user.json";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Registration = () => {
   const nav = useNavigate();
+  const [name, setName] = useState(null);
+  const [phone, setPhone] = useState(null);
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [cpassword, setCPassword] = useState(null);
+  const [data, setData] = useState([]);
 
-  const register = () => {};
+  const register = () => {
+    if (password === cpassword) {
+      setData((data) => [
+        ...data,
+        {
+          name: name,
+          email: email,
+          password: password,
+          phone: phone,
+          picture: null,
+        },
+      ]);
+    } else {
+      toast.error("Passwords do not match");
+    }
+  };
+
+  useEffect(() => {
+    setData(JSON.parse(localStorage.getItem("user")));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(data));
+  }, [data]);
+
   return (
     <section className="bg-gray-50">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover={false}
+        theme="colored"
+      />
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen h-full lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Create your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" onSubmit={register}>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                  required=""
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -33,6 +91,22 @@ const Registration = () => {
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                   required=""
                   onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Phone
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                  required=""
+                  onChange={(e) => setPhone(e.target.value)}
                 />
               </div>
               <div>
@@ -73,8 +147,7 @@ const Registration = () => {
                 </div>
               </div>
               <button
-                type="button"
-                onClick={() => register()}
+                type="submit"
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
               >
                 Sign up
